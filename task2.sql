949cd8d0f9bf81c4e0c137e88f675f9a7e57ac42
Durga Prasad <84697619+Durgaprasad-2002@users.noreply.github.com>
2023-02-26 23:56:51 +0530

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,23 @@
+create database FootBall
+create TABLE Football_Venue (
+    venue_id INT PRIMARY KEY,
+    venue_name VARCHAR(50),
+    city_id INT,
+    capacity INT
+);
+INSERT INTO Football_Venue (venue_id, venue_name, city_id, capacity)
+VALUES
+    (20001, 'France', 10003, 82115),
+    (20002, 'Mexico', 10007, 74742),
+    (20003, 'Spain', 10009, 81325),
+    (20004, 'Russia', 10010, 91000),
+    (20005, 'England', 10012, 78009),
+    (20006, 'Japan', 10013, 72387),
+    (20007, 'Germany', 10015, 89500),
+    (20008, 'Brazil', 10018, 80055),
+    (20009, 'Italy', 10019, 90500),
+    (20010, 'USA', 10020, 56698);
+SELECT COUNT(*) FROM Football_Venue;
+SELECT CONCAT(venue_name, ' ') AS Location, CONCAT(capacity, '') AS Volume FROM Football_Venue;
+DELETE FROM Football_Venue WHERE venue_name = 'England';
+select * from football_Venue
